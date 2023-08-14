bae8cb74f21a1d5bb3e66d591a9341b8f9f157fc
SerginhoLeal <sergiojunioleal@hotmail.com>
2023-08-14 16:23:07 -0300

    [settings]

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,21 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      'babel-plugin-styled-components',
+      [
+        'module-resolver',
+        {
+          root:['./src'],
+          extensions: ['.tsx', '.ts', '.js', '.json'],
+          alias: {
+            '@common': './src/common',
+            '@interface': './src/interface',
+            // '@svg': './src/assets/svg',
+            '@services': './src/services',
+          }
+        }
+      ]
+    ]
   };
 };
