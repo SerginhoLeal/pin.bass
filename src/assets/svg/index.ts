3e8f18da8a669067ecb652b18376990bad46656a
SerginhoLeal <sergiojunioleal@hotmail.com>
2023-08-19 13:24:44 -0300

    [new] adding svgs

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,17 @@
+import Settings from './setting.svg';
+import ArrowLeft from './arrow-left.svg';
+import RotateLeft from './rotate-left.svg';
+import ArrowRight from './arrow-right.svg';
+import RotateRight from './rotate-right.svg';
+import SearchNormal from './search-normal.svg';
+import Tag from './tag.svg';
+
+export {
+  Settings,
+  ArrowLeft,
+  RotateLeft,
+  RotateRight,
+  SearchNormal,
+  ArrowRight,
+  Tag
+}
